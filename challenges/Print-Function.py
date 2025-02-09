5763a1ed1ab7262a6abc851e12af111cba88a8c2
Sir_Jick <safaiypourabolfazl@gmail.com>
2025-02-09 08:09:04 +0330

    set doc string for main function

--- challenges/Print-Function.py
+++ challenges/Print-Function.py
@@ -2,9 +2,12 @@ from __future__ import print_function
 
 
 def main(n):
+    '''Prints all integers from 1 to n (inclusive) without any separators'''
     for i in range(int(n)):
         print(str(i+1), end='')
 
 
 if __name__ == '__main__':
     main(input())
+
+# https://www.hackerrank.com/challenges/python-print/problem
